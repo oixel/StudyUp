ed1a7885ab73890bdbbdaa875f4d53ea9d3e2205
DivyanshiSaini <sainid@ufl.edu>
2025-01-25 22:51:18 -0500

    Study Set selector Works! Needs to be linked still

--- StudySquid/app/screens/setSelector.js
+++ StudySquid/app/screens/setSelector.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground,  TouchableOpacity} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import  { useFonts } from 'expo-font';
 import { Raleway_400Regular } from '@expo-google-fonts/raleway';
 import { Quicksand_400Regular } from '@expo-google-fonts/quicksand';
 
 
-const setSelector = ({ screen, setScreen, players, setPlayers }) => {
-  
+const setSelector = () => {
+    const [focused, setFocused] = useState(-1);
+    const navigation = useNavigation(); // Initialize navigation
 
     const [fontsLoaded] = useFonts({
         Raleway_400Regular,
@@ -23,12 +25,57 @@ const setSelector = ({ screen, setScreen, players, setPlayers }) => {
     }
     
     return (
-            <View style={styles.container}>
-                <Text style={styles.title}>Select Study Set</Text>
+        <>
+         <ImageBackground
+                source={require('../../assets/images/FullStartScreen.png')}
+                style={styles.backgroundImage}
+                onTouchStart={() => setFocused(-1)}
+            />
+         <View style={styles.container}>
+                <Text style={styles.title}>Select Study Set!</Text>
+
+                {/* Buttons for each study set */}
+                {['SWAMPHACK TRIVIA!', 'Anatomy', 'Data Structure', 'Zoology'].map((set, index) => (
+                    <TouchableOpacity key={index} onPress={() => navigation.navigate('TargetScreen')}>
+                        <Text style={styles.text}>{set}</Text>
+                    </TouchableOpacity>
+                ))}
             </View>
+        </>
+            
         
     );
 }
 
 //just a comment
 export default setSelector;
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        gap: 12,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    title: {
+        alignItems: 'center',
+        fontFamily: 'PixelGame',
+        fontSize: 50,
+        marginBottom: 30,
+    },
+    text: {
+        paddingHorizontal: 8,
+        borderWidth: 2,
+        borderColor: '#20232a',
+        borderRadius: 6,
+        fontFamily: 'PixelGame',
+        fontSize: 30,
+        backgroundColor: '#F1EBB8',
+    },
+    backgroundImage: {
+        position: 'absolute',
+        width: "100%",
+        height: "100%",
+    }
+
+});
