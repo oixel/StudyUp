cdb5db1deeb669a50156021a6881d185be760a96
DivyanshiSaini <sainid@ufl.edu>
2025-01-26 04:03:28 -0500

    Result UI

--- StudySquid/app/screens/Results.js
+++ StudySquid/app/screens/Results.js
@@ -1,19 +1,40 @@
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native'
 import React from 'react'
 
 const Results = ({ screen, setScreen, results, setResults }) => {
     return (
         <View>
-            <Text>Results</Text>
-            <Button
-                onPress={() => { setScreen(screen + 1) }}
-                title="To Leaderboard =>"
-                color='white'
-            />
+            <Text style={styles.title}> Results! </Text>
+            <TouchableOpacity style={styles.button} onPress={() => { setScreen(screen + 1) }}>
+                <Text style={styles.buttonText}>Tap to view Leader Board!</Text>
+                </TouchableOpacity>
         </View>
     )
 }
 
 export default Results
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    title: {
+        display: 'flex',
+        gap: 12,
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontFamily: 'PixelGame',
+        fontSize: 100,
+        
+    },
+    buttonText: {
+        marginTop: 40,
+        paddingTop: 4,
+        textAlign: 'center',
+        borderWidth: 2,
+        borderColor: '#20232a',
+        borderRadius: 6,
+        fontFamily: 'PixelGame',
+        fontSize: 30,
+        backgroundColor: '#ffdacc',
+
+      }
+
+})
