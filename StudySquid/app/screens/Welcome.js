516e1fb699f3e58e94badb1cab050e6ae5159037
Krithika Kondapalli <krithika.kondapalli@gmail.com>
2025-01-26 00:22:29 -0500

    Add welcome screen button

--- StudySquid/app/screens/Welcome.js
+++ StudySquid/app/screens/Welcome.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, ImageBackground, Button} from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, TouchableOpacity} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import  { useFonts } from 'expo-font';
 
@@ -10,7 +11,8 @@ import { Quicksand_400Regular } from '@expo-google-fonts/quicksand';
 
 
 const WelcomeScreen = ( { setScreen }) => {
-    const[focused, setFocused] = useState(-1);
+    const [focused, setFocused] = useState(-1);
+    const navigation = useNavigation(); // Initialize navigation
 
     const [fontsLoaded] = useFonts({
         Raleway_400Regular,
@@ -37,6 +39,9 @@ const WelcomeScreen = ( { setScreen }) => {
                 <Text style={styles.title}>Welcome to</Text>
                 <Text style={styles.title2}>Study</Text>
                 <Text style={styles.title2}>Up!</Text>
+                <TouchableOpacity style={styles.button} onPress={() => console.log('Change to Lobby')}>
+                    <Text style={styles.buttonText}>Start!</Text>
+                </TouchableOpacity>
             </View>
         </>
     );
@@ -68,5 +73,22 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%',
     },
+    button: {
+        marginTop: 35,
+        width: 200,
+        height: 55,
+        borderColor: "rgb(77, 91, 111)",
+        borderRadius: 25,
+        borderWidth: 2,
+        backgroundColor: "rgb(220, 187, 207)",
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+    buttonText:{
+        marginTop: 4,
+        fontFamily: 'PixelGame',
+        color: "rgb(77, 91, 111)",
+        fontSize: 50
+    }
 });
-
