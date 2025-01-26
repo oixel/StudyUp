62b2c9257231953eb79e56a76d9172bb2f597199
oickle <oycell@aol.com>
2025-01-26 04:22:15 -0500

    Start result functionality

--- StudySquid/app/screens/Results.js
+++ StudySquid/app/screens/Results.js
@@ -1,12 +1,16 @@
 import { StyleSheet, Text, View, Button } from 'react-native'
 import React from 'react'
 
-const Results = ({ screen, setScreen, results, setResults }) => {
+import QuestionBar from "./QuestionBar";
+
+const Results = ({ screen, setScreen, questionSet, results, setResults }) => {
+    const resultEntries = Object.entries(results);
+    console.log(resultEntries);
+
     return (
         <View>
-            <Text>Results</Text>
             <Button
-                onPress={() => { setScreen(screen + 1) }}
+                onPress={() => { setResults({}); setScreen(screen + 1); }} // Wipe previous player's results and move to leaderboard
                 title="To Leaderboard =>"
                 color='white'
             />
