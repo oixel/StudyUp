fbad4adc7615ac58da64cb45d1ce53de2b02b562
oickle <oycell@aol.com>
2025-01-25 23:09:55 -0500

    Add functionality to move between pages

--- StudySquid/app/index.tsx
+++ StudySquid/app/index.tsx
@@ -1,14 +1,17 @@
 import { Text, View } from "react-native";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-import Lobby from "./screens/Lobby"
-import Roulette from "./screens/Roulette"
-import HeadsUp from "./screens/HeadsUp"
+import Lobby from "./screens/Lobby";
+import SetSelector from "./screens/SetSelector";
+import Roulette from "./screens/Roulette";
 import TaptheScreenToStart from "./screens/TaptheScreenToStart";
+import HeadsUp from "./screens/HeadsUp";
+import Results from "./screens/Results";
 
 export default function Index() {
   const [screen, setScreen] = useState(1);
   const [players, setPlayers] = useState([]);
+  const [questionSet, setQuestionSet] = useState([]);
 
   return (
     <View
@@ -16,14 +19,16 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-      }}
+      }
+      }
     >
       {/* {screen == 0 && <Welcome />} */}
       {screen == 1 && <Lobby screen={screen} setScreen={setScreen} players={players} setPlayers={setPlayers} />}
-      {/* {screen == 2 && <SetList />} */}
-      {screen == 2 && <Roulette players={players} />}
-      {screen == 3 && <HeadsUp isActive={screen == 3} />}
+      {screen == 2 && <SetSelector screen={screen} setScreen={setScreen} questionSet={questionSet} setQuestionSet={setQuestionSet} />}
+      {screen == 3 && <Roulette players={players} />}
       {screen == 4 && <TaptheScreenToStart />}
+      {screen == 5 && <HeadsUp screen={screen} setScreen={setScreen} isActive={screen == 5} />}
+      {screen == 6 && <Results />}
     </View>
   );
 }
