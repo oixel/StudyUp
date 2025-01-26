e0edbf2d25d1b17a0e6b3e015c4169ad70d1f8f1
DivyanshiSaini <sainid@ufl.edu>
2025-01-26 01:59:08 -0500

    Answer Board

---
+++ StudySquid/app/screens/AnswerBoard.js
@@ -0,0 +1,7 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+
+const HeadsUp = ({ screen, setScreen, isActive, questionSet, players, setPlayers, currentPlayer }) => {
+    
+}
+
