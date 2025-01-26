b825443c2d0ece0d63f61aa4779ce17e1e6e5d24
oickle <oycell@aol.com>
2025-01-25 21:09:14 -0500

    Cleaned up code

--- StudySquid/app/screens/Lobby.js
+++ StudySquid/app/screens/Lobby.js
@@ -5,7 +5,7 @@ import PlayerBox from "./lobby/PlayerBox";
 import AddPlayerButton from "./lobby/AddPlayerButton";
 import StartButton from "./lobby/StartButton";
 
-import  { useFonts } from 'expo-font';
+import { useFonts } from 'expo-font';
 
 import { Raleway_400Regular } from '@expo-google-fonts/raleway';
 import { Quicksand_400Regular } from '@expo-google-fonts/quicksand';
@@ -26,7 +26,7 @@ const Lobby = ({ screen, setScreen, players, setPlayers }) => {
     if (!fontsLoaded) {
         return <Text>Loading fonts...</Text>;
     }
-    
+
     return (
         <>
             <ImageBackground
@@ -56,13 +56,9 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     title: {
-<<<<<<< HEAD
-        fontSize: 48
-=======
         fontFamily: 'PixelGame',
         fontSize: 100,
         marginBottom: -25
->>>>>>> bbae11add2d0b5710b44e2c9405d8798399a6d14
     },
     backgroundImage: {
         position: 'absolute',
